fcc86bcb82d5bfcb321dce565f8edaa6ab2ec9e1
NickRamsey6 <nramseysc@gmail.com>
2024-06-11 09:18:13 -0600

    basic fizzbuzz

--- Chapter03/Operators/obj/Debug/net8.0/Operators.AssemblyInfo.cs
+++ Chapter03/Operators/obj/Debug/net8.0/Operators.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c022fa4dbb647529a1eeca3c55efc9c076146c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189ff3e64362fe30e5854167a9bcc2fd4e885568")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
