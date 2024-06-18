4316ddbdce85ca546391a0d94231f8dca390ff35
NickRamsey6 <nramseysc@gmail.com>
2024-06-18 14:45:52 -0600

    Ch6 Mutability

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -254,3 +254,22 @@ WriteLine("{0} is a valid e-mail address: {1}",
     arg1: email2.IsValidEmail());
 
 #endregion
+
+#region Mutability and records
+
+C1 c1 = new() { Name = "Bob" };
+c1.Name = "Bill";
+
+C2 c2 = new(Name: "Bob");
+//c2.Name = "Bill"; // CS8852: Init-only property
+
+S1 s1 = new() { Name = "Bob" };
+s1.Name = "Bill";
+
+S2 s2 = new(Name: "Bob");
+s2.Name = "Bill";
+
+S3 s3 = new(Name: "Bob");
+//s3.Name = "Bill"; // CS8852: Init-only property
+
+#endregion
