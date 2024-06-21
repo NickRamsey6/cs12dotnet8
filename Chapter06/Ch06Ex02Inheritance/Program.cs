24848bd2dfdfa7f874eb379c3a91285696d08c25
NickRamsey6 <nramseysc@gmail.com>
2024-06-21 15:30:22 -0600

    Experimenting with inheritance

--- Chapter06/Ch06Ex02Inheritance/Program.cs
+++ Chapter06/Ch06Ex02Inheritance/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Packt.Shared;
 
-Rectangle r = new(height: 3, width: 4.5);
+Rectangle r = new(3, 4.5);
 Console.WriteLine($"Rectangle H: {r.Height}, W: {r.Width}, Area: {r.Area}.");
 
 Square s = new(5);
@@ -8,3 +8,6 @@ Console.WriteLine($"Square H: {s.Height}, W: {s.Width}, Area: {s.Area}");
 
 Circle c = new(radius: 2.5);
 Console.WriteLine($"Circle H: {c.Height}, W: {c.Width}, Area: {c.Area}");
+
+Shape sh = r;
+Console.WriteLine($"Shape H:{sh.Height}, W:{sh.Width},A: {sh.Area}");
--- Chapter07/README.md
+++ Chapter07/README.md
@@ -2,6 +2,12 @@
 
 ## Key Concepts
 
+## Example Assemblies and Their Namespaces
+| Assembly | Example Namespaces | Example Types |
+|----------|--------------------|---------------|
+| System.Runtime.dll | System, System.Collections, System.Collections.Generic | This defines a comparison method that a type implements to order or sort its instances. |
+
+
 ## Projects List
 * AsembliesAndNamespaces
 
