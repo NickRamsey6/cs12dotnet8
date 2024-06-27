3e0f935f5139a41aafc03b5c8da522b7b0863061
NickRamsey6 <nramseysc@gmail.com>
2024-06-27 16:21:48 -0600

    Ch10 Laoding pattern queries

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -4,10 +4,12 @@
 //WriteLine($"Provider: {db.Database.ProviderName}");
 // Disposes the database context
 
-ConfigureConsole();
-QueryingCategories();
-FilteredIncludes();
-QueryingProducts();
-GettingOneProduct();
-QueryingWithLike();
-GetRandomProduct();
+//ConfigureConsole();
+//QueryingCategories();
+//FilteredIncludes();
+//QueryingProducts();
+//GettingOneProduct();
+//QueryingWithLike();
+//GetRandomProduct();
+
+LazyLoadingWithNoTracking();
