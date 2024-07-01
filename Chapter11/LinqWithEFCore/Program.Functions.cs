a757cf855a3a4c1ab2e0a2086c266ea64cf017e7
NickRamsey6 <nramseysc@gmail.com>
2024-07-01 14:47:22 -0600

    Ch11 build an EF core model and filter and sort sequences

---
+++ Chapter11/LinqWithEFCore/Program.Functions.cs
@@ -0,0 +1,39 @@
+﻿using Northwind.EntityModels; // To use NorthwindDb, Category, Product
+using Microsoft.EntityFrameworkCore; // To use DbSet<T>
+
+partial class Program
+{
+    private static void FilterAndSort()
+    {
+        SectionTitle("Filter and sort");
+
+        using NorthwindDb db = new();
+
+        DbSet<Product> allProducts = db.Products;
+
+        IQueryable<Product> filteredProducts = allProducts.Where(product => product.UnitPrice < 10M);
+
+        IOrderedQueryable<Product> sortedAndFilteredProducts = filteredProducts.OrderByDescending(product => product.UnitPrice);
+
+        // Extend LINQ query to use the Select method to return only three properties that we need
+        var projectedProducts = sortedAndFilteredProducts
+            .Select(product => new // Anonymous type
+            {
+                product.ProductId,
+                product.ProductName,
+                product.UnitPrice
+            });
+
+        WriteLine("Products that cost less than $10:");
+
+        //Output the SQL
+        WriteLine(projectedProducts.ToQueryString());
+        //foreach (Product p in sortedAndFilteredProducts)
+        foreach (var p in projectedProducts)
+        {
+            WriteLine("{0}: {1} costs {2:$#, ##0.00}",
+                p.ProductId, p.ProductName, p.UnitPrice);
+        }
+        WriteLine();
+    }
+}
