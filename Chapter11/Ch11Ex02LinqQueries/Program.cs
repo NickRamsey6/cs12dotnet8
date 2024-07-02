dbf0c1d63a3485cd244ae893f7a2fffc006f7814
NickRamsey6 <nramseysc@gmail.com>
2024-07-02 12:05:00 -0600

    Ch11 bonus excercise additional functionality

--- Chapter11/Ch11Ex02LinqQueries/Program.cs
+++ Chapter11/Ch11Ex02LinqQueries/Program.cs
@@ -2,7 +2,16 @@
 
 using NorthwindDb db = new();
 
-Write("Enter the name of a city: ");
+IQueryable<string> distinctCities = db.Customers.Select(c => c.City).Distinct()!;
+
+WriteLine($"We do business in {distinctCities.Count()} cities.");
+WriteLine("Here are all the cities we do business in:");
+foreach (string city in distinctCities)
+{
+    WriteLine(city);
+}
+
+Write("Enter the name of a city to filter on: ");
 string inputCity = ReadLine()!;
 WriteLine();
 
