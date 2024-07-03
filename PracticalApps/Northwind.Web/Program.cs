151505ebdd44d495dff67dbf84f5c1a3471706ef
NickRamsey6 <nramseysc@gmail.com>
2024-07-03 11:54:01 -0600

    Northwind.web project setup

---
+++ PracticalApps/Northwind.Web/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+#region
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => 
+    $"Environment is {app.Environment.EnvironmentName}");
+
+#endregion
+
+
+// Start the web server, host the website, and wait for requests.
+app.Run(); // This ia a thread-blocking call
+WriteLine("This executes after the web server has stopped!");
