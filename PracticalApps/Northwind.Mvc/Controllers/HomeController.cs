2495848a0e50db4b187f272bb4f6f0ea35f4ed2a
NickRamsey6 <nramseysc@gmail.com>
2024-07-11 10:03:49 -0600

    MVC model binding

--- PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
+++ PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -62,5 +62,24 @@ namespace Northwind.Mvc.Controllers
 
             return View(model); // Pass model to view and then return result
         }
+
+        // This action method will handle GET and other requests except POST
+        public IActionResult ModelBinding()
+        {
+            return View(); // The page with a form to submit
+        }
+
+        [HttpPost] // This action method will handle POST requests
+        public IActionResult ModelBinding(Thing thing)
+        {
+            HomeModelBindingViewModel model = new(
+                Thing: thing, HasErrors: !ModelState.IsValid,
+                ValidationErrors: ModelState.Values
+                    .SelectMany(state => state.Errors)
+                    .Select(error => error.ErrorMessage)
+            );
+
+            return View(model); // Show the model bound thing
+        }
     }
 }
